d6e4f1d05bc562570d97d275582dab3522f1a5da
Matt W <uranazo@gmail.com>
2013-07-19 21:31:12 -0400

    Capistrano assets and bundling...

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,10 @@ set :user, :deployer
 set :use_sudo, false
 set :deploy_to, "/var/www/#{application}"
 set :deploy_via, :remote_cache
+set :default_shell, "/bin/bash -l"
 
 role :web, "192.81.210.122"                          # Your HTTP server, Apache/etc
 role :app, "192.81.210.122"                          # This may be the same as your `Web` server
 role :db,  "192.81.210.122", :primary => true # This is where Rails migrations will run
+
+before 'deploy:setup', 'rvm:install_rvm'
