380dcdeffc46d039c184070263807b1774271098
Matt W <uranazo@gmail.com>
2013-07-19 23:57:29 -0400

    Fixing unicorn start/restarts

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ listen "127.0.0.1:8080"
 worker_processes 1
 user "deployer"
 working_directory "/var/www/gemador.com/current/"
-pid "/home/unicorn/pids/unicorn.pid"
+pid "/var/www/gemador.com/current/tmp/pids/unicorn.pid"
 stderr_path "/home/unicorn/log/unicorn.log"
 stdout_path "/home/unicorn/log/unicorn.log"
 preload_app true
