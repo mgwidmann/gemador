73ad665f8a5d70e82db06ca06eb982b087547a59
Matt W <uranazo@gmail.com>
2013-10-01 22:49:24 -0400

    Update sale price to show member price when not logged in...

--- extensions/spree_on_sale/app/overrides/display_sale_price.rb
+++ extensions/spree_on_sale/app/overrides/display_sale_price.rb
@@ -6,9 +6,15 @@ Deface::Override.new(virtual_path: 'spree/products/_cart_form',
                         <small>
                           ORIGINAL PRICE<br/>
                           <span class="price selling small">
-                            <%= @product.price_in(current_currency, true).display_price %>
+                            <%= @product.price_in(current_currency, :original).display_price %>
                           </span>
                         </small>
+                      <% else %>
+                        <small>
+                        MEMBER PRICE - <%= link_to 'Sign up today!', spree_signup_path %><br/>
+                        <span class="price selling small">
+                          <%= @product.price_in(current_currency, :sale).display_price %>
+                        </span>
                       <% end %>
                       PARTIAL
                       )
